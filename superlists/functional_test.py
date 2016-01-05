b54a2ef2cdd531b0609306889c3effaf62fdcb0c
Demo User <demouser@MacBook-Pro.local>
2016-01-05 11:21:31 -0500

    Expanded ellas story and functional tests

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,19 +14,33 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Ella goes to check out a cool new website
         #She goes to the home page
-
         self.browser.get('http://localhost:8000')
 
         #She notices the the page title and the header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #she is inivted to enter a to-do list item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         #she types water the plants into a text box
+        inputbox.send_keys('Water the plants')
 
         #when she hits enter the page refreashes and displays a list
         # "1 : Water the plants"
+        inputbox.send_keys(Keys.ENTER)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Water the plants' for row in rows)
+        )
+        
         #There is still a text box prompting Ella to enter in another item
         #she enters "Run a mile" into the text box and presses enter
 
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -12,7 +12,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-
-        self.assertTrue(response.content.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith('</html>'))
+        expected_html = render_to_sting('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
