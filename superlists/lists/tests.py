d5e879e9baa8d0d80c3a27204b901510e7192423
Demo User <demouser@MacBook-Pro.local>
2016-01-08 11:42:24 -0500

    Users have unique URL and can add to their own lists

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -37,8 +37,33 @@ class NewListsTest(TestCase):
             data={'item_text': 'A new list item'}
         )
 
-        self.assertRedirects(response, '/lists/the-only-list/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        # other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id),
+            data = {'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id),
+            data = {'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
 class ListViewTest(TestCase):
 
@@ -47,14 +72,26 @@ class ListViewTest(TestCase):
         Item.objects.create(text= 'itemey 1', list = new_list)
         Item.objects.create(text= 'itemey 2', list = new_list)
 
-        response = self.client.get('/lists/the-only-list/')
+        other_list = List.objects.create()
+        Item.objects.create(text= 'other item 1', list = other_list)
+        Item.objects.create(text= 'other item 2', list = other_list)
+
+        response = self.client.get('/lists/%d/' % (new_list.id,))
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response,  'other item 1')
+        self.assertNotContains(response,  'other item 2')
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list/')
+        new_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (new_list.id,))
         self.assertTemplateUsed(response, 'list.html')
+
+    def test_passes_correct_list_to_template(self):
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
 
 
 class ItemAndListModelsTest(TestCase):
